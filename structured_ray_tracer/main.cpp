1b729cb84355cad7726516c4ff389ad5d7ec0dcf
o.olsson@uq.edu.au <o.olsson@uq.edu.au>
2017-04-13 15:57:04 +1000

    - improved the structured_ray_tracer example a bit.

--- structured_ray_tracer/main.cpp
+++ structured_ray_tracer/main.cpp
@@ -45,28 +45,63 @@ class Object;
 std::vector<Object*> g_objects;
 
 // Data types:
+
+/**
+ * The Camera structure contains all the information needed to describe the camera model.
+ */
 struct Camera
 {
-	int height;
 	int width;
-	vec3 viewDir;
-	vec3 viewLeft;
-	vec3 viewUp;
+	int height;
+	vec3 dir;
+	vec3 left;
+	vec3 up;
 	vec3 position;
 	float fovY;
 	float aspectRatio;
 };
 
+/**
+ * Initializes the Camera structure. Calculates all derived properties of the camera.
+ */
+Camera makeCamera(int screenWidth, int screenHeight, const vec3 &cameraPos, const vec3 &cameraTarget, const vec3 &cameraUp, float  verticalFOV)
+{
+	Camera camera;
+	camera.width = screenWidth;
+	camera.height = screenHeight;
 
+	camera.position = cameraPos;
+	// 1. Form camera forwards vector
+	camera.dir = normalize(cameraTarget - cameraPos);
+	// 2. Figure out image plane sideways (left?) direction
+	camera.left = normalize(cross(cameraUp, camera.dir));
+	// 3. Figure out image up direction
+	camera.up = cross(camera.dir, camera.left);
+	camera.aspectRatio = float(camera.width) / float(camera.height);
+	camera.fovY = verticalFOV;
+
+	return camera;
+}
+
+/**
+ * Structure representing a parametric ray with an origin and a direction.
+ */
 struct Ray
 {
 	vec3 origin;
 	vec3 direction;
 };
 
-
+/**
+ * Structure information about a hit point. By default initialized to represent not having hit anything.
+ * We chose to repreesnt this using the maximum number floats can representation.
+ * This struct could be extended with more information about the hit point, for example the normal is usuall very useful, and instead of the object
+ * maybe representing the material (an object might have several materials) is more useful, or maybe just a function object that returns the shading?
+ */
 struct HitInfo
 {
+	HitInfo() : time(std::numeric_limits<float>::max()), object(nullptr) { }
+
 	float time;
 	Object *object;
 };
@@ -79,6 +114,10 @@ class Object
 public:
 	//std::function<HitInfo(const Ray &ray)> intersect;
 
+	/**
+	 * intersect is a pure virtual function, it must be overridden in all derived classes. Derive to implent different object types
+	 * (see Sphere below).
+	 */
 	virtual HitInfo intersect(const Ray &ray) = 0;
 
 	vec3 colour; // This is sensibly replaced with a more comprehensive material class, or some such that takes care of the shading calculation!
@@ -117,14 +156,20 @@ inline bool intersectRaySphere(const vec3 &rayO, const vec3 &rayD, const vec3 &s
 }
 
 
-// Implementation of sphere object type
+/**
+ * Implementation of sphere object type
+ */
 class Sphere : public Object
 {
 public:
+
+	/**
+	 * Overrides the base implementation for a sphere. 
+	 */
 	virtual HitInfo intersect(const Ray &ray) override
 	{
+		// Initially a miss!
 		HitInfo hit;
-		hit.time = std::numeric_limits<float>::max();
 		float t = 0.0f;
 
 		if (intersectRaySphere(ray.origin, ray.direction, position, radius, t))
@@ -135,13 +180,32 @@ public:
 		return hit;
 	}
 
+	// Data for representing a sphere.
 	vec3 position;
 	float radius;
 };
 
+/**
+* Helper function to make a sphere.
+* (Yes, allocating using new and returning sets someone else up to deal with the memory, leak-prone! std::unique_ptr/shared_ptr might be the answer)
+*/
+Sphere *makeSphere(const vec3 &position, float radius, const vec3 &colour)
+{
+	Sphere *sphere = new Sphere;
+
+	sphere->position = position;
+	sphere->radius = radius;
+	sphere->colour = colour;
+
+	return sphere;
+}
 
 
-Ray generatePrimaryRay(int x, int y, const Camera &c)
+/**
+ * Generates a ray through the pixel (x,y). The ray has unit length and origin at the camera position.
+ * Uses the pin-hole camera model, to change the model we could just generate a different distribution.
+ */
+Ray generatePinHolePrimaryRay(int x, int y, const Camera &c)
 {
 	Ray r;
 
@@ -149,22 +213,26 @@ Ray generatePrimaryRay(int x, int y, const Camera &c)
 
 	r.origin = c.position;
 
-	r.direction = normalize(c.viewDir +
-		tanf(degreesToRadians(c.fovY / 2.0f)) * c.viewUp * pixelNormCoord.y +
-		tanf(degreesToRadians(c.fovY / 2.0f)) * c.aspectRatio * c.viewLeft * pixelNormCoord.x);
+	r.direction = normalize(c.dir +
+		tanf(degreesToRadians(c.fovY / 2.0f)) * c.up * pixelNormCoord.y +
+		tanf(degreesToRadians(c.fovY / 2.0f)) * c.aspectRatio * c.left* pixelNormCoord.x);
 
 	return r;
 }
 
-
+/**
+ * Traces a ray through the scene (here represented by a list of objects), returns information about the intersection point.
+ * In a recursive ray tracer this information would include the shading at the intersection point.
+ */
 HitInfo trace(const Ray &ray, const std::vector<Object*> &objects)
 {
 	HitInfo best;
-	best.object = nullptr;
-	best.time = std::numeric_limits<float>::max();
+
 	for (auto o : objects)
 	{
 		HitInfo h = o->intersect(ray);
+
+		// Check if new hit time is better
 		if (h.time < best.time)
 		{
 			best = h;
@@ -174,7 +242,12 @@ HitInfo trace(const Ray &ray, const std::vector<Object*> &objects)
 	return best;
 }
 
-
+/**
+ * This funciton is called to calculate shading. It should really be dependent on the material of the object hit,
+ * and it should absolutely not use 'g_spherePos' etc.
+ * In a recursive ray tracer it might want to call trace again to sample shadows, for example, 
+ * although to make this neat, would require a bit more re-shuffling (e.g., trace needs to call shade etc...).
+ */
 vec3 shade(const Ray &ray, const HitInfo &hit)
 {
 	float objectDistance = length(ray.origin - g_spherePos);
@@ -182,30 +255,21 @@ vec3 shade(const Ray &ray, const HitInfo &hit)
 	return hit.object->colour * shading;
 }
 
+
 // Callback that is called by GLUT system when a frame needs to be drawn, set up in main() using 'glutDisplayFunc'
 static void onGlutDisplay()
 {
-	Camera camera;
-	camera.height = glutGet(GLUT_WINDOW_HEIGHT);
-	camera.width = glutGet(GLUT_WINDOW_WIDTH);
-
-	camera.position = g_viewPosition;
-	// 1. Form camera forwards vector
-	camera.viewDir = normalize(g_viewTarget - g_viewPosition);
-	// 2. Figure out image plane sideways (left?) direction
-	camera.viewLeft = normalize(cross(g_viewUp, camera.viewDir));
-	// 3. Figure out image up direction
-	camera.viewUp = cross(camera.viewDir, camera.viewLeft);
-	camera.aspectRatio = float(camera.width) / float(camera.height);
-	camera.fovY = g_fov;
+	Camera camera = makeCamera(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT), g_viewPosition, g_viewTarget, g_viewUp, g_fov);
 
 	// memory to use to store the pixels to
 	std::vector<vec3> pixels(camera.width * camera.height, g_backGroundColour);
+	
+	// loop over all the pixel locations.
 	for (int y = 0; y < camera.height; ++y)
 	{
 		for (int x = 0; x < camera.width; ++x)
 		{
-			Ray r = generatePrimaryRay(x, y, camera);
+			Ray r = generatePinHolePrimaryRay(x, y, camera);
 
 			HitInfo h = trace(r, g_objects);
 
@@ -216,13 +280,17 @@ static void onGlutDisplay()
 		}
 	}
 
-	// Must check for empty vector
+	// Must check for empty vector (if window is made 0 size in either dimension)
 	if (!pixels.empty())
 	{
+		// just copy the pixel data to the frame buffer.
 		glDrawPixels(camera.width, camera.height, GL_RGB, GL_FLOAT, &pixels[0]);
 	}
+
+	// tell GLUT to get the OS to swap the back and front buffers.
 	glutSwapBuffers();
 }
+
 
 
 
@@ -235,34 +303,14 @@ int main(int argc, char* argv[])
 	glutInitWindowSize(g_startWidth, g_startHeight);
 	glutCreateWindow("A somewhat more structured and extensible ray tracer");
 
-	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
 	glutSwapBuffers();
 
 	printf("--------------------------------------\nOpenGL\n  Vendor: %s\n  Renderer: %s\n  Version: %s\n--------------------------------------\n", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
 
-
 	// Set up scene:
-	Sphere sphere;
-	sphere.colour = g_sphereColour;
-	sphere.position = g_spherePos - vec3(3.2f, 0.0f, 0.0f);;
-	sphere.radius = g_sphereRadius / 2.0f;
-
-	g_objects.push_back(&sphere);
-
-	Sphere sphere2;
-	sphere2.colour = vec3(0.2f, 0.9f, 0.3f);
-	sphere2.position = g_spherePos + vec3(0.0f, 2.0f, 0.0f);
-	sphere2.radius = g_sphereRadius / 2.0f;
-
-	g_objects.push_back(&sphere2);
-
-	Sphere sphere3;
-	sphere3.colour = vec3(0.8f, 0.4f, 0.1f);
-	sphere3.position = g_spherePos + vec3(3.2f, 0.0f, 0.0f);
-	sphere3.radius = g_sphereRadius / 2.0f;
-
-	g_objects.push_back(&sphere3);
+	g_objects.push_back(makeSphere(g_spherePos - vec3(3.2f, 0.0f, 0.0f), g_sphereRadius / 2.0f, g_sphereColour));
+	g_objects.push_back(makeSphere(g_spherePos + vec3(0.0f, 2.0f, 0.0f), g_sphereRadius / 2.0f, vec3(0.2f, 0.9f, 0.3f)));
+	g_objects.push_back(makeSphere(g_spherePos + vec3(3.2f, 0.0f, 0.0f), g_sphereRadius / 2.0f, vec3(0.8f, 0.4f, 0.1f)));
 
 	glutDisplayFunc(onGlutDisplay);
 
